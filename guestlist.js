97b37d4da1e1c12221e16097599b8149e57d0adc
Dineen <35275339+dineensch@users.noreply.github.com>
2019-02-07 15:58:27 -0800

    added guestlist file

---
+++ guestlist.js
@@ -0,0 +1,52 @@
+export default {
+    "results":[
+        {
+            "id": 1,
+            "addGuest": true,
+            "first_name": "",
+            "last_name": "",
+            "guest_first": "",
+            "guest_last": "",
+            "rsvp": [],
+        },
+        {
+            "id": 2,
+            "addGuest": true,
+            "first_name": "",
+            "last_name": "",
+            "guest_first": "",
+            "guest_last": "",
+            "rsvp": [],
+        },
+        {
+            "id": 3,
+            "addGuest": true,
+            "first_name": "",
+            "last_name": "",
+            "guest_first": "",
+            "guest_last": "",
+            "rsvp": [],
+        },
+        {
+            "id": 4,
+            "addGuest": false,
+            "first_name": "",
+            "last_name": "",
+            "rsvp": [],
+        },
+        {
+            "id": 5,
+            "addGuest": false,
+            "first_name": "",
+            "last_name": "",
+            "rsvp": [],
+        },          
+        {
+            "id": 6,
+            "addGuest": false,
+            "first_name": "",
+            "last_name": "",
+            "rsvp": [],
+        },
+    ]
+}
