b7fd31de0084297fb780bb0f54007d7c2942352c
Dineen <35275339+dineensch@users.noreply.github.com>
2019-02-27 16:23:55 -0800

    added vuetify

--- src/main.js
+++ src/main.js
@@ -1,18 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from "vue";
-import App from "./App";
-import VueRouter from "vue-router";
-import router from "./router";
-import BootstrapVue from "bootstrap-vue";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+import Vue from "vue"
+import App from "./App"
+import Vuetify from 'vuetify'
+import VueRouter from "vue-router"
+import router from "./router"
+import 'vuetify/dist/vuetify.min.css'
+import BootstrapVue from "bootstrap-vue"
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
 
 
-Vue.config.productionTip = false;
-Vue.use(VueRouter);
-Vue.use(BootstrapVue);
-Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false
+Vue.use(Vuetify)
+Vue.use(VueRouter)
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVue)
 
 // Initialize Firebase
 // TODO: Only import part of the SDK that you'll use
