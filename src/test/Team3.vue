d2a3b2191be14c34d1c03b5811c90c0fcfba2bd6
Dineen <35275339+dineensch@users.noreply.github.com>
2019-03-18 17:02:34 -0700

    added content to Team page

---
+++ src/test/Team3.vue
@@ -0,0 +1,197 @@
+<template>
+	<div class="mb-5">
+		<nav-bar></nav-bar>
+		<page-hero>
+			<template v-slot:title>The Team</template>
+			<template v-slot:body>Meet the team.</template>
+		</page-hero>
+		<!-- TEMPLATE TEST -->
+		<b-container class="mt-5">
+			<b-row v-bind="rowStyle">
+				<!-- MASTER ROW -->
+				<b-col v-bind="masterCol">
+					<h2>Side 1</h2>
+					<b-row v-bind="rowStyle">
+						<b-col v-bind="colStyle">
+							<div v-bind="red">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=1011" alt="headshot"/>
+								<h5>Rachel</h5>
+							</div>
+						</b-col>
+						<b-col v-bind="colStyle">
+							<div v-bind="lightGrey">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=823" alt="headshot"/>
+								<h5>Taylor</h5>
+							</div>
+						</b-col>
+					</b-row>
+
+					<b-row v-bind="rowStyle">
+						<b-col v-bind="colStyle">
+							<div v-bind="beige">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=1005" alt="headshot"/>
+								<h5>Carly</h5>
+							</div>
+						</b-col>
+
+						<b-col v-bind="colStyle">
+							<div v-bind="red">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=836" alt="headshot"/>
+								<h5>Sarah</h5>
+							</div>
+						</b-col>
+					</b-row>
+
+					<b-row v-bind="rowStyle">
+						<b-col v-bind="colStyle">
+							<div v-bind="grey">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=777" alt="headshot"/>
+								<h5>Kelsey</h5>
+							</div>
+						</b-col>
+						<b-col v-bind="colStyle">
+							<div v-bind="drkGrey">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=821" alt="headshot"/>
+								<h5>Ali</h5>
+							</div>
+						</b-col>
+						<b-col v-bind="colStyle">
+							<div v-bind="beige">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=821" alt="headshot"/>
+								<h5>Hilary</h5>
+							</div>
+						</b-col>
+					</b-row>
+				</b-col>
+				<b-col v-bind="masterCol">
+					<h2>Side 2</h2>
+					<b-row v-bind="rowStyle">
+						<b-col v-bind="colStyle">
+							<div v-bind="grey">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=1012" alt="headshot"/>
+								<h5>Jeremy</h5>
+							</div>
+						</b-col>
+
+						<b-col v-bind="colStyle">
+							<div v-bind="beige">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=883" alt="headshot"/>
+								<h5>Jaydee</h5>
+							</div>
+						</b-col>
+					</b-row>
+
+					<b-row v-bind="rowStyle">
+						<b-col v-bind="colStyle">
+							<div v-bind="drkGrey">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=863" alt="headshot"/>
+								<h5>Tucker</h5>
+							</div>
+						</b-col>
+
+						<b-col v-bind="colStyle">
+							<div v-bind="lightGrey">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=856" alt="headshot"/>
+								<h5>CJ</h5>
+							</div>
+						</b-col>
+					</b-row>
+					<b-row v-bind="rowStyle">
+						<b-col v-bind="colStyle">
+							<div v-bind="beige">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=845" alt="headshot"/>
+								<h5>Mike</h5>
+							</div>
+						</b-col>
+						<b-col v-bind="colStyle">
+							<div v-bind="red">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=804" alt="headshot"/>
+								<h5>Matt</h5>
+							</div>
+						</b-col>
+					</b-row>
+				</b-col>
+			</b-row>
+			<!-- END MASTER ROW -->
+		</b-container>
+	</div>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar";
+import PageHero from "@/components/PageHero";
+
+export default {
+	name: "TheTeam",
+	components: {
+		NavBar,
+		PageHero
+	},
+	data() {
+		return {
+			photoStyle: { rounded: "circle", class: "img-fluid" },
+			colStyle: {
+				class: "p-0 text-center justify-content-center",
+				cols: "12",
+				md: "6",
+				lg: "6"
+			},
+			rowStyle: { class: "p-0 m-0" },
+			masterCol: { class: "p-0 text-center", cols: "6" },
+			red: { class: "red p-2 m-1" },
+			beige: { class: "beige p-2 m-1" },
+			lightGrey: { class: "light-grey p-2 m-1" },
+			grey: { class: "grey p-2 m-1" },
+			drkGrey: { class: "drk-grey p-2 m-1" }
+		};
+	},
+	methods: {
+		mouseOver: function() {
+			this.active = !this.active;
+		}
+	}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.red {
+	background-color: #633637;
+	color: white;
+}
+
+.beige {
+	background-color: #ebdbd3;
+}
+
+.light-grey {
+	background-color: #dadada;
+}
+
+.grey {
+	background-color: #635c5c;
+	color: white;
+}
+
+.drk-grey {
+	background-color: #363436;
+	color: white;
+}
+
+h2,
+h5 {
+	text-align: center !important;
+	font-family: brandon-grotesque, sans-serif;
+}
+
+h2 {
+	font-weight: 500;
+}
+
+h5 {
+	font-weight: 300;
+	margin: 0;
+}
+</style>
+
+
--- src/views/TheTeam.vue
+++ src/views/TheTeam.vue
@@ -1,93 +1,138 @@
 <template>
-  <div class="mb-5">
-    <nav-bar></nav-bar>
-    <page-hero>
-      <template v-slot:title>The Team</template>
-      <template v-slot:body>Meet the wedding party.</template>
-    </page-hero>
-    <b-container>
-      <b-row>
-        <!-- COLUMN 1 -->
-        <b-col>
-          <div id="flip">
-            <transition name="card" mode="out-in">
-              <!-- FRONT OF CARD -->
-              <div class="card" v-if="front == true" key="front">
-                <b-button class="btn-flip" @click="front = false">
-                  <div class="card-content">
-                    <b-img
-                      rounded="circle"
-                      src="https://picsum.photos/300/300/?image=41"
-                      alt="image"
-                      fluid
-                      class="mb-3"
-                    />
-                    <h3>Jane Doe</h3>
-                    <span>View More</span>
-                  </div>
-                </b-button>
-              </div>
-              <!-- BACK OF CARD -->
-              <div class="card" v-else key="back">
-                <b-button class="btn-flip" @click="front = true">
-                  <div class="card-content">
-                    <b-img
-                      rounded="circle"
-                      src="https://picsum.photos/300/300/?image=41"
-                      alt="image"
-                      fluid
-                      class="mb-3"
-                    />
-                    <h3>Back</h3>
-                    <span>View More</span>
-                  </div>
-                </b-button>
-              </div>
-            </transition>
-          </div>
-        </b-col>
-        <b-col>
-          <div id="flip">
-            <transition name="card" mode="out-in">
-              <!-- FRONT OF CARD -->
-              <div class="card" v-if="front == true" key="front">
-                <b-button class="btn-flip" @click="front = false">
-                  <div class="card-content">
-                    <b-img
-                      rounded="circle"
-                      src="https://picsum.photos/300/300/?image=41"
-                      alt="image"
-                      fluid
-                      class="mb-3"
-                    />
-                    <h3>Jane Doe</h3>
-                    <span>View More</span>
-                  </div>
-                </b-button>
-              </div>
-              <!-- BACK OF CARD -->
-              <div class="card" v-else key="back">
-                <b-button class="btn-flip" @click="front = true">
-                  <div class="card-content">
-                    <b-img
-                      rounded="circle"
-                      src="https://picsum.photos/300/300/?image=41"
-                      alt="image"
-                      fluid
-                      class="mb-3"
-                    />
-                    <h3>Back</h3>
-                    <span>View More</span>
-                  </div>
-                </b-button>
-              </div>
-            </transition>
-          </div>
-        </b-col>
-      </b-row>
-    </b-container>
-  </div>
-  <!--END CARD 1 -->
+	<div class="mb-5">
+		<nav-bar></nav-bar>
+		<page-hero>
+			<template v-slot:title>The Team</template>
+			<template v-slot:body>Meet the wedding party.</template>
+		</page-hero>
+		<!-- TEMPLATE TEST -->
+		<b-container class="mt-5">
+			<!-- MASTER ROW -->
+			<b-row v-bind="rowStyle">
+				<!-- 1st Column -->
+				<b-col v-bind="masterCol">
+					<h2>Bridesmaids</h2>
+					<b-row v-bind="rowStyle">
+						<b-col v-bind="colStyle" lg="12">
+							<div v-bind="red" id="exPopover1">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=1011" alt="headshot"/>
+								<h5>Rachel</h5>
+								<b-popover
+									content="This Photo Placeholder of Rachel enjoys paddling this canoe and wearing life vests"
+									title="Maid of Honor"
+									triggers="hover focus"
+									target="exPopover1"
+									placement="top"
+								/>
+							</div>
+						</b-col>
+						<b-col v-bind="colStyle" lg="6">
+							<div v-bind="beige" id="exPopover2">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=823" alt="headshot"/>
+								<h5>Taylor</h5>
+								<b-popover
+									content="This Photo Placeholder of Taylor enjoys red hats and taking photos of you while you takea photo of her"
+									title="Taylor"
+									triggers="hover focus"
+									target="exPopover2"
+									placement="top"
+								/>
+							</div>
+						</b-col>
+
+						<b-col v-bind="colStyle" lg="6">
+							<div v-bind="lightGrey">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=1005" alt="headshot"/>
+								<h5>Carly</h5>
+							</div>
+						</b-col>
+
+						<b-col v-bind="colStyle" lg="12">
+							<div v-bind="drkGrey">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=836" alt="headshot"/>
+								<h5>Kelsey</h5>
+							</div>
+						</b-col>
+
+						<b-col v-bind="colStyle" lg="6">
+							<div v-bind="red">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=777" alt="headshot"/>
+								<h5>Sarah</h5>
+							</div>
+						</b-col>
+						<b-col v-bind="colStyle" lg="6">
+							<div v-bind="lightGrey">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=821" alt="headshot"/>
+								<h5>Ali</h5>
+							</div>
+						</b-col>
+						<b-col v-bind="colStyle" lg="12">
+							<div v-bind="beige">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=786" alt="headshot"/>
+								<h5>Hilary</h5>
+							</div>
+						</b-col>
+					</b-row>
+				</b-col>
+				<!-- 2nd Column -->
+				<b-col v-bind="masterCol">
+					<h2>Groomsmen</h2>
+					<b-row v-bind="rowStyle">
+						<b-col v-bind="colStyle" lg="12">
+							<div v-bind="grey">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=1012" alt="headshot"/>
+								<h5>Jeremy</h5>
+							</div>
+						</b-col>
+
+						<b-col v-bind="colStyle" lg="6">
+							<div v-bind="beige">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=883" alt="headshot"/>
+								<h5>Jaydee</h5>
+							</div>
+						</b-col>
+
+						<b-col v-bind="colStyle" lg="6">
+							<div v-bind="drkGrey">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=863" alt="headshot"/>
+								<h5>Tucker</h5>
+							</div>
+						</b-col>
+
+						<b-col v-bind="colStyle">
+							<div v-bind="lightGrey">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=856" alt="headshot"/>
+								<h5>CJ</h5>
+							</div>
+						</b-col>
+						<b-col v-bind="colStyle" lg="6">
+							<div v-bind="beige">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=845" alt="headshot"/>
+								<h5>Mike</h5>
+							</div>
+						</b-col>
+						<b-col v-bind="colStyle" lg="6" order="12" order-sm="12" order-lg="6">
+							<div v-bind="blankText">
+								<b-img
+									v-bind="[photoStyle, imgShape]"
+									src="https://picsum.photos/200/200/?image=804"
+									alt="headshot"
+								/>
+								<h5>Blank</h5>
+							</div>
+						</b-col>
+						<b-col v-bind="colStyle" order-lg="12">
+							<div v-bind="red">
+								<b-img v-bind="photoStyle" src="https://picsum.photos/200/200/?image=804" alt="headshot"/>
+								<h5>Matt</h5>
+							</div>
+						</b-col>
+					</b-row>
+				</b-col>
+			</b-row>
+			<!-- END MASTER ROW -->
+		</b-container>
+	</div>
 </template>
 
 <script>
@@ -95,76 +140,79 @@ import NavBar from "@/components/NavBar";
 import PageHero from "@/components/PageHero";
 
 export default {
-  name: "TheTeam",
-  components: {
-    NavBar,
-    PageHero
-  },
-  data() {
-    return {
-      front: true,
-      front2: true
-    };
-  },
-  methods: {
-    mouseOver: function() {
-      this.active = !this.active;
-    }
-  }
+	name: "TheTeam",
+	components: {
+		NavBar,
+		PageHero
+	},
+	data() {
+		return {
+			photoStyle: { rounded: "circle", class: "img-fluid" },
+			imgShape: { blank: true, width: 200, height: 200 },
+			blankText: { class: "blank-text p-2 m-1" },
+			colStyle: {
+				class: "p-0 text-center justify-content-center",
+				cols: "12",
+				md: "12"
+			},
+			rowStyle: { class: "p-0 m-0" },
+			masterCol: { class: "p-0 text-center", cols: "6" },
+			red: { class: "red p-2 m-1" },
+			beige: { class: "beige p-2 m-1" },
+			lightGrey: { class: "light-grey p-2 m-1" },
+			grey: { class: "grey p-2 m-1" },
+			drkGrey: { class: "drk-grey p-2 m-1" }
+		};
+	},
+	methods: {
+		mouseOver: function() {
+			this.active = !this.active;
+		}
+	}
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/* .icon {
-	background: url("https://picsum.photos/300/300/?image=41");
-} */
-
-/* .card {
-	background-color: transparent;
-	border: none;
-} */
-
-#flip {
-  display: flex;
-  justify-content: center;
+.red {
+	background-color: #633637;
+	color: white;
 }
 
-.btn-flip {
-  color: none !important;
-  background-color: transparent !important;
-  border-color: transparent !important;
+.beige {
+	background-color: #ebdbd3;
 }
 
-.card {
-  text-align: center;
-  background-color: transparent !important;
-  border-color: transparent !important;
+.light-grey {
+	background-color: #dadada;
 }
 
-.card-content {
-  color: black;
-  /* background-color: rgba(0, 0, 0, 0.5); */
+.grey {
+	background-color: #635c5c;
+	color: white;
 }
 
-h1,
+.drk-grey {
+	background-color: #363436;
+	color: white;
+}
+.blank-text {
+	background-color: #635c5c;
+	color: #635c5c;
+}
+h2,
+h5 {
+	text-align: center !important;
+	font-family: brandon-grotesque, sans-serif;
+}
+
 h2 {
-  color: black;
+	font-weight: 500;
 }
 
-.txt-wh {
-  color: white;
-}
-
-.card-enter,
-.card-leave-to {
-  opacity: 0;
-  transform: rotateY(90deg);
-}
-
-.card-enter-active,
-.card-leave-active {
-  transition: all 0.5s;
+h5 {
+	font-weight: 300;
+	margin: 0;
 }
 </style>
 
